d19adfac0758428e6a1e66653ecf0cf326fa75a7
BenC2026 <BCroyle1@my.gcu.edu>
2025-03-16 16:16:28 -0700

    Changed 2D array to 3 separate arrays

--- Assets/Markov.cs
+++ Assets/Markov.cs
@@ -4,7 +4,9 @@ public class Markov : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
-    public float[,] graph = { { 0.25f, 0.35f, 0.5f }, { 0.45f, 0.3f, 0.65f }, { 0.75f, 0.2f, 0.15f } };
+    public float[] Cola = { 0.25f, 0.35f, 0.5f };
+    public float[] Lemonade = { 0.45f, 0.3f, 0.65f };
+    public float[] Orange = { 0.75f, 0.2f, 0.15f };
     void Start()
     {
         
