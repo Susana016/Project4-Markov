a6776ceb10bd6c0f7a3f78a4c69f1234bf078599
BenC2026 <BCroyle1@my.gcu.edu>
2025-03-19 11:06:30 -0700

    Added more bottles

--- Assets/Markov.cs
+++ Assets/Markov.cs
@@ -1,15 +1,33 @@
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class Markov : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
-    public float[] Cola = { 0.25f, 0.35f, 0.5f };
-    public float[] Lemonade = { 0.45f, 0.3f, 0.65f };
-    public float[] Orange = { 0.75f, 0.2f, 0.15f };
+    public float[] Cola = { 0.25f, 0.35f, 0.4f };
+    public float[] Lemonade = { 0.45f, 0.1f, 0.45f };
+    public float[] Orange = { 0.75f, 0.05f, 0.2f };
     void Start()
     {
-        
+        float sum = 0;
+        for (int i = 0; i < Cola.Length; i++)
+        {
+            sum += Cola[i];
+        }
+        Debug.Log("Sum of items in Cola is " + sum);
+        sum = 0;
+        for (int i = 0; i < Lemonade.Length; i++)
+        {
+            sum += Lemonade[i];
+        }
+        Debug.Log("Sum of items in Lemonade is " + sum);
+        sum = 0;
+        for (int i = 0; i < Orange.Length; i++)
+        {
+            sum += Orange[i];
+        }
+        Debug.Log("Sum of items in Orange is " + sum);
     }
 
     // Update is called once per frame
