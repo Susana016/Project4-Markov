925413a46847afc8034d20abae4d7203761c9a11
BenC2026 <BCroyle1@my.gcu.edu>
2025-04-02 09:18:37 -0700

    Added movement script

--- Assets/Markov.cs
+++ Assets/Markov.cs
@@ -1,33 +1,25 @@
 using JetBrains.Annotations;
+using UnityEditor;
 using UnityEngine;
 
 public class Markov : MonoBehaviour
 {
+    public Transform C1;
+    public Transform C2;
+    public Transform C3;
+    public Transform L1;
+    public Transform L2;
+    public Transform L3;
+    public Transform O1;
+    public Transform O2;
+    public Transform O3;
+    public float[] ColaGraph = { 0.25f, 0.35f, 0.4f };
+    public float[] LemonadeGraph = { 0.35f, 0.45f, 0.2f };
+    public float[] OrangeGraph = { 0.4f, 0.2f, 0.4f  };
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-
-    public float[] Cola = { 0.25f, 0.35f, 0.4f };
-    public float[] Lemonade = { 0.45f, 0.1f, 0.45f };
-    public float[] Orange = { 0.75f, 0.05f, 0.2f };
     void Start()
     {
-        float sum = 0;
-        for (int i = 0; i < Cola.Length; i++)
-        {
-            sum += Cola[i];
-        }
-        Debug.Log("Sum of items in Cola is " + sum);
-        sum = 0;
-        for (int i = 0; i < Lemonade.Length; i++)
-        {
-            sum += Lemonade[i];
-        }
-        Debug.Log("Sum of items in Lemonade is " + sum);
-        sum = 0;
-        for (int i = 0; i < Orange.Length; i++)
-        {
-            sum += Orange[i];
-        }
-        Debug.Log("Sum of items in Orange is " + sum);
+        
     }
 
     // Update is called once per frame
@@ -36,16 +28,40 @@ public class Markov : MonoBehaviour
         
     }
 
+    public float[,] getGraph()
+    {
+        float[,] graph = new float[,] { { C1.position.y, C2.position.y, C3.position.y }, { L1.position.y, L2.position.y, L3.position.y }, {O1.position.y, O2.position.y, O3.position.y } };
+        return graph;
+    }
+
     public float[] getCola()
     {
-        return Cola;
+        return ColaGraph;
     }
-    public float[] getLemonade() 
-    { 
-        return Lemonade; 
+
+    public float[] getLemonade()
+    {
+        return LemonadeGraph;
     }
+
     public float[] getOrange()
     {
-        return Orange;
+        return OrangeGraph;
+    }
+
+    public float[,] changeGraph()
+    {
+        float[,] graph = { { ColaGraph[0], ColaGraph[1], ColaGraph[2] }, { LemonadeGraph[0], LemonadeGraph[1], LemonadeGraph[2] }, { OrangeGraph[0], OrangeGraph[1], OrangeGraph[2] } };
+        float[,] newGraph = new float[,] { {C1.position.y, C2.position.y, C3.position.y }, {L1.position.y, L2.position.y, L3.position.y }, {O1.position.y, O2.position.y, O3.position.y } };
+        newGraph[0, 0] = newGraph[0, 0] * graph[0, 0] + newGraph[0, 1] * graph[1, 0] + newGraph[0, 2] * graph[2, 0];
+        newGraph[0, 1] = newGraph[0, 0] * graph[0, 1] + newGraph[0, 1] * graph[1, 1] + newGraph[0, 2] * graph[2, 1];
+        newGraph[0, 2] = newGraph[0, 0] * graph[0, 2] + newGraph[0, 1] * graph[1, 2] + newGraph[0, 2] * graph[2, 2];
+        newGraph[1, 0] = newGraph[1, 0] * graph[0, 0] + newGraph[1, 1] * graph[1, 0] + newGraph[1, 2] * graph[2, 0];
+        newGraph[1, 1] = newGraph[1, 0] * graph[0, 1] + newGraph[1, 1] * graph[1, 1] + newGraph[1, 2] * graph[2, 1];
+        newGraph[1, 2] = newGraph[1, 0] * graph[0, 2] + newGraph[1, 1] * graph[1, 2] + newGraph[1, 2] * graph[2, 2];
+        newGraph[2, 0] = newGraph[2, 0] * graph[0, 0] + newGraph[2, 1] * graph[1, 0] + newGraph[2, 2] * graph[2, 0];
+        newGraph[2, 1] = newGraph[2, 0] * graph[0, 1] + newGraph[2, 1] * graph[1, 1] + newGraph[2, 2] * graph[2, 1];
+        newGraph[2, 2] = newGraph[2, 0] * graph[0, 2] + newGraph[2, 1] * graph[1, 2] + newGraph[2, 2] * graph[2, 2];
+        return newGraph;
     }
 }
--- Assets/Soda.cs
+++ Assets/Soda.cs
@@ -1,16 +1,130 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Soda : MonoBehaviour
 {
+    public Markov Markov;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (transform.position.x == -8)
+        {
+            transform.position = new Vector3(transform.position.x, (Markov.getCola()[0]), transform.position.z);
+        }
+        if (transform.position.x == -6)
+        {
+            transform.position = new Vector3(transform.position.x, (Markov.getLemonade()[0]), transform.position.z);
+        }
+        if (transform.position.x == -4)
+        {
+            transform.position = new Vector3(transform.position.x, (Markov.getOrange()[0]), transform.position.z);
+        }
+        if (transform.position.x == -2)
+        {
+            transform.position = new Vector3(transform.position.x, (Markov.getCola()[1]), transform.position.z);
+        }
+        if (transform.position.x == 0)
+        {
+            transform.position = new Vector3(transform.position.x, (Markov.getLemonade()[1]), transform.position.z);
+        }
+        if (transform.position.x == 2)
+        {
+            transform.position = new Vector3(transform.position.x, (Markov.getOrange()[1]), transform.position.z);
+        }
+        if (transform.position.x == 4)
+        {
+            transform.position = new Vector3(transform.position.x, (Markov.getCola()[2]), transform.position.z);
+        }
+        if (transform.position.x == 6)
+        {
+            transform.position = new Vector3(transform.position.x, (Markov.getLemonade()[2]), transform.position.z);
+        }
+        if (transform.position.x == 8)
+        {
+            transform.position = new Vector3(transform.position.x, (Markov.getOrange()[2]), transform.position.z);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            if (transform.position.x == -8)
+            {
+                transform.position = new Vector3(transform.position.x, (Markov.changeGraph()[0, 0]), transform.position.z);
+            }
+            if (transform.position.x == -6)
+            {
+                transform.position = new Vector3(transform.position.x, (Markov.changeGraph()[0, 1]), transform.position.z);
+            }
+            if (transform.position.x == -4)
+            {
+                transform.position = new Vector3(transform.position.x, (Markov.changeGraph()[0, 2]), transform.position.z);
+            }
+            if (transform.position.x == -2)
+            {
+                transform.position = new Vector3(transform.position.x, (Markov.changeGraph()[1, 0]), transform.position.z);
+            }
+            if (transform.position.x == 0)
+            {
+                transform.position = new Vector3(transform.position.x, (Markov.changeGraph()[1, 1]), transform.position.z);
+            }
+            if (transform.position.x == 2)
+            {
+                transform.position = new Vector3(transform.position.x, (Markov.changeGraph()[1, 2]), transform.position.z);
+            }
+            if (transform.position.x == 4)
+            {
+                transform.position = new Vector3(transform.position.x, (Markov.changeGraph()[2, 0]), transform.position.z);
+            }
+            if (transform.position.x == 6)
+            {
+                transform.position = new Vector3(transform.position.x, (Markov.changeGraph()[2, 1]), transform.position.z);
+            }
+            if (transform.position.x == 8)
+            {
+                transform.position = new Vector3(transform.position.x, (Markov.changeGraph()[2, 2]), transform.position.z);
+            }
+        }
+        else
+        {
+            if (transform.position.x == -8)
+            {
+                transform.position = new Vector3(transform.position.x, (Markov.getGraph()[0, 0]), transform.position.z);
+            }
+            if (transform.position.x == -6)
+            {
+                transform.position = new Vector3(transform.position.x, (Markov.getGraph()[0, 1]), transform.position.z);
+            }
+            if (transform.position.x == -4)
+            {
+                transform.position = new Vector3(transform.position.x, (Markov.getGraph()[0, 2]), transform.position.z);
+            }
+            if (transform.position.x == -2)
+            {
+                transform.position = new Vector3(transform.position.x, (Markov.getGraph()[1, 0]), transform.position.z);
+            }
+            if (transform.position.x == 0)
+            {
+                transform.position = new Vector3(transform.position.x, (Markov.getGraph()[1, 1]), transform.position.z);
+            }
+            if (transform.position.x == 2)
+            {
+                transform.position = new Vector3(transform.position.x, (Markov.getGraph()[1, 2]), transform.position.z);
+            }
+            if (transform.position.x == 4)
+            {
+                transform.position = new Vector3(transform.position.x, (Markov.getGraph()[2, 0]), transform.position.z);
+            }
+            if (transform.position.x == 6)
+            {
+                transform.position = new Vector3(transform.position.x, (Markov.getGraph()[2, 1]), transform.position.z);
+            }
+            if (transform.position.x == 8)
+            {
+                transform.position = new Vector3(transform.position.x, (Markov.getGraph()[2, 2]), transform.position.z);
+            }
+        }
     }
 }
