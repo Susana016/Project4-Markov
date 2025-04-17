624870e5bb7776e09c671eae7920b7d271b82168
BenC2026 <BCroyle1@my.gcu.edu>
2025-04-17 11:59:48 -0700

    Debug

--- Assets/Soda.cs
+++ Assets/Soda.cs
@@ -53,38 +53,83 @@ public class Soda : MonoBehaviour
             if (transform.position.x == -8)
             {
                 transform.position = new Vector3(transform.position.x, (Markov.changeGraph()[0, 0]), transform.position.z);
+                if (transform.position.y >= 1)
+                {
+                    Debug.Log("The market is cola-dominant.");
+                    UnityEditor.EditorApplication.isPlaying = false;
+                }
             }
             if (transform.position.x == -6)
             {
                 transform.position = new Vector3(transform.position.x, (Markov.changeGraph()[0, 1]), transform.position.z);
+                if (transform.position.y >= 1)
+                {
+                    Debug.Log("The market is lemonade-dominant.");
+                    UnityEditor.EditorApplication.isPlaying = false;
+                }
             }
             if (transform.position.x == -4)
             {
                 transform.position = new Vector3(transform.position.x, (Markov.changeGraph()[0, 2]), transform.position.z);
+                if (transform.position.y >= 1)
+                {
+                    Debug.Log("The market is orange-dominant.");
+                    UnityEditor.EditorApplication.isPlaying = false;
+                }
             }
             if (transform.position.x == -2)
             {
                 transform.position = new Vector3(transform.position.x, (Markov.changeGraph()[1, 0]), transform.position.z);
+                if (transform.position.y >= 1)
+                {
+                    Debug.Log("The market is cola-dominant.");
+                    UnityEditor.EditorApplication.isPlaying = false;
+                }
             }
             if (transform.position.x == 0)
             {
                 transform.position = new Vector3(transform.position.x, (Markov.changeGraph()[1, 1]), transform.position.z);
+                if (transform.position.y >= 1)
+                {
+                    Debug.Log("The market is lemonade-dominant.");
+                    UnityEditor.EditorApplication.isPlaying = false;
+                }
             }
             if (transform.position.x == 2)
             {
                 transform.position = new Vector3(transform.position.x, (Markov.changeGraph()[1, 2]), transform.position.z);
+                if (transform.position.y >= 1)
+                {
+                    Debug.Log("The market is orange-dominant.");
+                    UnityEditor.EditorApplication.isPlaying = false;
+                }
             }
             if (transform.position.x == 4)
             {
                 transform.position = new Vector3(transform.position.x, (Markov.changeGraph()[2, 0]), transform.position.z);
+                if (transform.position.y >= 1)
+                {
+                    Debug.Log("The market is cola-dominant.");
+                    UnityEditor.EditorApplication.isPlaying = false;
+                }
             }
             if (transform.position.x == 6)
             {
                 transform.position = new Vector3(transform.position.x, (Markov.changeGraph()[2, 1]), transform.position.z);
+                if (transform.position.y >= 1)
+                {
+                    Debug.Log("The market is lemonade-dominant.");
+                    UnityEditor.EditorApplication.isPlaying = false;
+                }
             }
             if (transform.position.x == 8)
             {
                 transform.position = new Vector3(transform.position.x, (Markov.changeGraph()[2, 2]), transform.position.z);
+                if (transform.position.y >= 1)
+                {
+                    Debug.Log("The market is orange-dominant.");
+                    UnityEditor.EditorApplication.isPlaying = false;
+                }
             }
         }
         else if (Input.GetKeyDown(KeyCode.C))
@@ -92,38 +137,83 @@ public class Soda : MonoBehaviour
             if (transform.position.x == -8)
             {
                 transform.position = new Vector3(transform.position.x, (Markov.changeGraphCola()[0, 0]), transform.position.z);
+                if (transform.position.y >= 1)
+                {
+                    Debug.Log("The market is cola-dominant.");
+                    UnityEditor.EditorApplication.isPlaying = false;
+                }
             }
             if (transform.position.x == -6)
             {
                 transform.position = new Vector3(transform.position.x, (Markov.changeGraphCola()[0, 1]), transform.position.z);
+                if (transform.position.y >= 1)
+                {
+                    Debug.Log("The market is lemonade-dominant.");
+                    UnityEditor.EditorApplication.isPlaying = false;
+                }
             }
             if (transform.position.x == -4)
             {
                 transform.position = new Vector3(transform.position.x, (Markov.changeGraphCola()[0, 2]), transform.position.z);
+                if (transform.position.y >= 1)
+                {
+                    Debug.Log("The market is orange-dominant.");
+                    UnityEditor.EditorApplication.isPlaying = false;
+                }
             }
             if (transform.position.x == -2)
             {
                 transform.position = new Vector3(transform.position.x, (Markov.changeGraphCola()[1, 0]), transform.position.z);
+                if (transform.position.y >= 1)
+                {
+                    Debug.Log("The market is cola-dominant.");
+                    UnityEditor.EditorApplication.isPlaying = false;
+                }
             }
             if (transform.position.x == 0)
             {
                 transform.position = new Vector3(transform.position.x, (Markov.changeGraphCola()[1, 1]), transform.position.z);
+                if (transform.position.y >= 1)
+                {
+                    Debug.Log("The market is lemonade-dominant.");
+                    UnityEditor.EditorApplication.isPlaying = false;
+                }
             }
             if (transform.position.x == 2)
             {
                 transform.position = new Vector3(transform.position.x, (Markov.changeGraphCola()[1, 2]), transform.position.z);
+                if (transform.position.y >= 1)
+                {
+                    Debug.Log("The market is orange-dominant.");
+                    UnityEditor.EditorApplication.isPlaying = false;
+                }
             }
             if (transform.position.x == 4)
             {
                 transform.position = new Vector3(transform.position.x, (Markov.changeGraphCola()[2, 0]), transform.position.z);
+                if (transform.position.y >= 1)
+                {
+                    Debug.Log("The market is cola-dominant.");
+                    UnityEditor.EditorApplication.isPlaying = false;
+                }
             }
             if (transform.position.x == 6)
             {
                 transform.position = new Vector3(transform.position.x, (Markov.changeGraphCola()[2, 1]), transform.position.z);
+                if (transform.position.y >= 1)
+                {
+                    Debug.Log("The market is lemonade-dominant.");
+                    UnityEditor.EditorApplication.isPlaying = false;
+                }
             }
             if (transform.position.x == 8)
             {
                 transform.position = new Vector3(transform.position.x, (Markov.changeGraphCola()[2, 2]), transform.position.z);
+                if (transform.position.y >= 1)
+                {
+                    Debug.Log("The market is orange-dominant.");
+                    UnityEditor.EditorApplication.isPlaying = false;
+                }
             }
         }
         else if (Input.GetKeyDown(KeyCode.L))
@@ -131,38 +221,83 @@ public class Soda : MonoBehaviour
             if (transform.position.x == -8)
             {
                 transform.position = new Vector3(transform.position.x, (Markov.changeGraphLemonade()[0, 0]), transform.position.z);
+                if (transform.position.y >= 1)
+                {
+                    Debug.Log("The market is cola-dominant.");
+                    UnityEditor.EditorApplication.isPlaying = false;
+                }
             }
             if (transform.position.x == -6)
             {
                 transform.position = new Vector3(transform.position.x, (Markov.changeGraphLemonade()[0, 1]), transform.position.z);
+                if (transform.position.y >= 1)
+                {
+                    Debug.Log("The market is lemonade-dominant.");
+                    UnityEditor.EditorApplication.isPlaying = false;
+                }
             }
             if (transform.position.x == -4)
             {
                 transform.position = new Vector3(transform.position.x, (Markov.changeGraphLemonade()[0, 2]), transform.position.z);
+                if (transform.position.y >= 1)
+                {
+                    Debug.Log("The market is orange-dominant.");
+                    UnityEditor.EditorApplication.isPlaying = false;
+                }
             }
             if (transform.position.x == -2)
             {
                 transform.position = new Vector3(transform.position.x, (Markov.changeGraphLemonade()[1, 0]), transform.position.z);
+                if (transform.position.y >= 1)
+                {
+                    Debug.Log("The market is cola-dominant.");
+                    UnityEditor.EditorApplication.isPlaying = false;
+                }
             }
             if (transform.position.x == 0)
             {
                 transform.position = new Vector3(transform.position.x, (Markov.changeGraphLemonade()[1, 1]), transform.position.z);
+                if (transform.position.y >= 1)
+                {
+                    Debug.Log("The market is lemonade-dominant.");
+                    UnityEditor.EditorApplication.isPlaying = false;
+                }
             }
             if (transform.position.x == 2)
             {
                 transform.position = new Vector3(transform.position.x, (Markov.changeGraphLemonade()[1, 2]), transform.position.z);
+                if (transform.position.y >= 1)
+                {
+                    Debug.Log("The market is orange-dominant.");
+                    UnityEditor.EditorApplication.isPlaying = false;
+                }
             }
             if (transform.position.x == 4)
             {
                 transform.position = new Vector3(transform.position.x, (Markov.changeGraphLemonade()[2, 0]), transform.position.z);
+                if (transform.position.y >= 1)
+                {
+                    Debug.Log("The market is cola-dominant.");
+                    UnityEditor.EditorApplication.isPlaying = false;
+                }
             }
             if (transform.position.x == 6)
             {
                 transform.position = new Vector3(transform.position.x, (Markov.changeGraphLemonade()[2, 1]), transform.position.z);
+                if (transform.position.y >= 1)
+                {
+                    Debug.Log("The market is lemonade-dominant.");
+                    UnityEditor.EditorApplication.isPlaying = false;
+                }
             }
             if (transform.position.x == 8)
             {
                 transform.position = new Vector3(transform.position.x, (Markov.changeGraphLemonade()[2, 2]), transform.position.z);
+                if (transform.position.y >= 1)
+                {
+                    Debug.Log("The market is orange-dominant.");
+                    UnityEditor.EditorApplication.isPlaying = false;
+                }
             }
         }
         else if (Input.GetKeyDown(KeyCode.O))
@@ -170,38 +305,83 @@ public class Soda : MonoBehaviour
             if (transform.position.x == -8)
             {
                 transform.position = new Vector3(transform.position.x, (Markov.changeGraphOrange()[0, 0]), transform.position.z);
+                if (transform.position.y >= 1)
+                {
+                    Debug.Log("The market is cola-dominant.");
+                    UnityEditor.EditorApplication.isPlaying = false;
+                }
             }
             if (transform.position.x == -6)
             {
                 transform.position = new Vector3(transform.position.x, (Markov.changeGraphOrange()[0, 1]), transform.position.z);
+                if (transform.position.y >= 1)
+                {
+                    Debug.Log("The market is lemonade-dominant.");
+                    UnityEditor.EditorApplication.isPlaying = false;
+                }
             }
             if (transform.position.x == -4)
             {
                 transform.position = new Vector3(transform.position.x, (Markov.changeGraphOrange()[0, 2]), transform.position.z);
+                if (transform.position.y >= 1)
+                {
+                    Debug.Log("The market is orange-dominant.");
+                    UnityEditor.EditorApplication.isPlaying = false;
+                }
             }
             if (transform.position.x == -2)
             {
                 transform.position = new Vector3(transform.position.x, (Markov.changeGraphOrange()[1, 0]), transform.position.z);
+                if (transform.position.y >= 1)
+                {
+                    Debug.Log("The market is cola-dominant.");
+                    UnityEditor.EditorApplication.isPlaying = false;
+                }
             }
             if (transform.position.x == 0)
             {
                 transform.position = new Vector3(transform.position.x, (Markov.changeGraphOrange()[1, 1]), transform.position.z);
+                if (transform.position.y >= 1)
+                {
+                    Debug.Log("The market is lemonade-dominant.");
+                    UnityEditor.EditorApplication.isPlaying = false;
+                }
             }
             if (transform.position.x == 2)
             {
                 transform.position = new Vector3(transform.position.x, (Markov.changeGraphOrange()[1, 2]), transform.position.z);
+                if (transform.position.y >= 1)
+                {
+                    Debug.Log("The market is orange-dominant.");
+                    UnityEditor.EditorApplication.isPlaying = false;
+                }
             }
             if (transform.position.x == 4)
             {
                 transform.position = new Vector3(transform.position.x, (Markov.changeGraphOrange()[2, 0]), transform.position.z);
+                if (transform.position.y >= 1)
+                {
+                    Debug.Log("The market is cola-dominant.");
+                    UnityEditor.EditorApplication.isPlaying = false;
+                }
             }
             if (transform.position.x == 6)
             {
                 transform.position = new Vector3(transform.position.x, (Markov.changeGraphOrange()[2, 1]), transform.position.z);
+                if (transform.position.y >= 1)
+                {
+                    Debug.Log("The market is lemonade-dominant.");
+                    UnityEditor.EditorApplication.isPlaying = false;
+                }
             }
             if (transform.position.x == 8)
             {
                 transform.position = new Vector3(transform.position.x, (Markov.changeGraphOrange()[2, 2]), transform.position.z);
+                if (transform.position.y >= 1)
+                {
+                    Debug.Log("The market is orange-dominant.");
+                    UnityEditor.EditorApplication.isPlaying = false;
+                }
             }
         }
         else
